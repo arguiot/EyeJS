41ed721af27058cb7a58c9a3285839252587945d
Arthur Guiot <arguiot@gmail.com>
2017-10-07 20:12:29 +0800

    Testing if EyeJS can run on Travis with OS X

---
+++ __test__/simple.js
@@ -0,0 +1,17 @@
+eye.test("Test", "node",
+	$ => $(1+2+3+4+5).Equal((5**2+5)/2)
+);
+
+eye.test("Browser", "browser", path.join(testFileDir, "index.html"))
+
+eye.test("Test2", "node",
+	$ => !$(true).Equal(false)
+);
+function sum(a, b) {
+	return a + b
+}
+
+eye.test("Sum", "node",
+	$ => $(sum(1, 2)).Equal(3),
+	$ => $(sum(0.2, 0.4)).isCloseTo(0.6, 1)
+)
--- dist/eye.js
+++ dist/eye.js
@@ -6,13 +6,13 @@
 
 
 // Require all modules
-const ora = require('ora');
-const colors = require('colors');
-const express = require('express');
-const open = require('openurl');
-const fs = require('fs');
-const path = require('path');
-const notifier = require('node-notifier');
+const ora = require("ora");
+const colors = require("colors");
+const express = require("express");
+const open = require("openurl");
+const fs = require("fs");
+const path = require("path");
+const notifier = require("node-notifier");
 
 // Class EyeJS
 class EyeJS {
@@ -21,35 +21,35 @@ class EyeJS {
 		const app = express();
 		const server = app.listen(3000, function () {
 			open.open("http://localhost:3000");
-		})
-		app.get('/', function (req, res) {
+		});
+		app.get("/", function (req, res) {
 			fs.readFile(path.isAbsolute(file[0]) ? file[0] : process.cwd() + "/" + file[0], (err, data) => {
-				res.send(data.toString('utf8'));
+				res.send(data.toString("utf8"));
 			});
-		})
-		app.get('/post/', function (req, res) {
+		});
+		app.get("/post/", function (req, res) {
 			const result = req.query.result;
 			const failed = req.query.failed;
 			if (result == 0) {
 				spinner.fail();
 				// failed += 1;
 				console.group();
-				console.log(`\n${failed} test(s) failed\n`.red)
+				console.log(`\n${failed} test(s) failed\n`.red);
 				console.groupEnd();
 				// console.log(this.data);
 			}
 			else if (result == 1) {
-				spinner.succeed()
+				spinner.succeed();
 			}
 			else {
-				spinner.warn()
+				spinner.warn();
 			}
-			res.send("sucess")
+			res.send("sucess");
 			server.close();
 		});
-		app.get('/js/', (req, res) => {
+		app.get("/js/", (req, res) => {
 			fs.readFile(__dirname + "/../dist/client.js", (err, data) => {
-				res.send(data.toString('utf8'));
+				res.send(data.toString("utf8"));
 			});
 		});
 	}
@@ -66,25 +66,25 @@ class EyeJS {
 				console.log("Passed:".bold, this.data.tested);
 				console.log("Failed:".bold.red, this.data.failed);
 				const end = process.hrtime(this.time);
-				const time = Math.round((end[0] * 1000) + (end[1] / 1000000))
+				const time = Math.round((end[0] * 1000) + (end[1] / 1000000));
 				console.log("Time".bold, `${time > 1000 ? time / 1000 + "s" : time + "ms"}`);
 				console.group();
 				console.log("✔ EyeJS exited with no critical errors".green);
 				notifier.notify({
-					title: 'EyeJS',
-					message: '✔ EyeJS exited with no critical errors',
-					icon: path.join(__dirname, '../docs/img/EyeJS-logo.png'),
-				})
+					title: "EyeJS",
+					message: "✔ EyeJS exited with no critical errors",
+					icon: path.join(__dirname, "../docs/img/EyeJS-logo.png"),
+				});
 				process.exit(0);
 			}
 			else {
 				console.log("\n");
 				console.log(`✖ Oups!, There is problem somewhere! Exited with ${code}`.red);
 				notifier.notify({
-					title: 'EyeJS - Error',
+					title: "EyeJS - Error",
 					message: `✖ Oups!, There is problem somewhere! Exited with ${code}`,
-					icon: path.join(__dirname, '../docs/img/EyeJS-logo.png'),
-				})
+					icon: path.join(__dirname, "../docs/img/EyeJS-logo.png"),
+				});
 				process.exit(1);
 			}
 		});
@@ -93,10 +93,10 @@ class EyeJS {
 		const $ = $ => {
 			class expect {
 				constructor(val) {
-					this.val = val
+					this.val = val;
 				}
 				Equal(val) {
-					return JSON.stringify(val) == JSON.stringify(this.val) ? true : false
+					return JSON.stringify(val) == JSON.stringify(this.val) ? true : false;
 				}
 				is(type) {
 					return typeof this.val == type ? true : false;
@@ -108,42 +108,42 @@ class EyeJS {
 					return callback(this.val);
 				}
 				Match(val) {
-					return val.test(this.val) == true ? true : false
+					return val.test(this.val) == true ? true : false;
 				}
 				toRun() {
 					try {
-						this.val()
+						this.val();
 					} catch (e) {
-						return false
+						return false;
 					}
-					return true
+					return true;
 				}
 			}
-			return new expect($)
-		}
+			return new expect($);
+		};
 		let result = !0;
 		let failed = [];
 		for (var i = 0; i < callbacks.length; i++) {
-			const temp = callbacks[i]($)
+			const temp = callbacks[i]($);
 			if (temp == !1) {
-				result = result == !0 || result == !1 ? false : result
-				failed.push(i + 1)
+				result = result == !0 || result == !1 ? false : result;
+				failed.push(i + 1);
 			} else if (temp != !1 && temp != !0) {
-				result = temp
+				result = temp;
 			}
 		}
 		if (result == !1) {
 			spinner.fail();
 			this.data.failed += 1;
 			console.group();
-			console.log(`\nTest ${failed} failed\n`.red)
+			console.log(`\nTest ${failed} failed\n`.red);
 			console.groupEnd();
 		}
 		else if (result == !0) {
-			spinner.succeed()
+			spinner.succeed();
 		}
 		else {
-			spinner.warn()
+			spinner.warn();
 		}
 	}
 	test(name, type) {
@@ -151,7 +151,7 @@ class EyeJS {
 		const spinner = ora(name).start();
 		let callbacks = [];
 		for (var i = 0; i < arguments.length - 2; i++) {
-			callbacks.push(arguments[i + 2])
+			callbacks.push(arguments[i + 2]);
 		}
 		return type == "browser" ? this.browser(name, spinner, callbacks) : this.node(name, spinner, callbacks);
 	}
@@ -161,11 +161,11 @@ if (typeof define === "function" && define.amd) {
 	define(() => new EyeJS);
 // CommonJS and Node.js module support.
 } else if (typeof exports !== "undefined") {
-		// Support Node.js specific `module.exports` (which can be a function)
+	// Support Node.js specific `module.exports` (which can be a function)
 	if (typeof module !== "undefined" && module.exports) {
 		exports = module.exports = new EyeJS;
 	}
-		// But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+	// But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
 	exports.DisplayJS = new EyeJS;
 } else if (typeof global !== "undefined") {
 	global.DisplayJS = new EyeJS;
