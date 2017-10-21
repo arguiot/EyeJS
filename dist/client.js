182c549a4dd659ed0bd7b7a83f89e0c372287aae
Arthur Guiot <arguiot@gmail.com>
2017-10-21 17:14:25 +0800

    :bug: Fixing client with new method

--- dist/client.js
+++ dist/client.js
@@ -144,19 +144,24 @@ class EyeJS {
     };
     let result = !0;
     let failed = [];
+    let tothrow = [];
     for (var i = 0; i < arguments.length; i++) {
       const callback = arguments[i];
       const temp = callback($);
-      if (temp == !1) {
-        result = result == !0 || result == !1 ? false : result;
+      if (temp == !1 || typeof temp == "string") {
+        result = false;
+        tothrow.push(temp);
         failed.push(i + 1);
       } else if (temp != !1 && temp != !0) {
         result = temp;
       }
     }
     if (result == !1) {
+      spinner.fail();
       this.data.failed += failed.length;
-      console.log(`\nTest ${failed} failed\n`);
+      for (i = 0; i < failed.length; i++) {
+        console.log(`\nTest ${failed[i]} failed: ${tothrow[i]}\n`.red);
+      }
     } else if (result == !0) {
       this.data.tested += 1;
     } else {
