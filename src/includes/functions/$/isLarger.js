c6311c4c07f72a570ab6774b5eb89cde0d650471
Arthur Guiot <arguiot@gmail.com>
2017-10-22 13:57:35 +0800

    :green_heart: Fixed build

--- src/includes/functions/$/isLarger.js
+++ src/includes/functions/$/isLarger.js
@@ -2,6 +2,6 @@ isLarger(val, not) {
 	if((this.val > val && not != false) || (not == false && !(this.val > val))) {
 		return true;
 	} else {
-		return `${this.val} isn't larger than ${actual}`
+		return `${this.val} isn't larger than ${val}`
 	}
 }
--- src/includes/functions/$/isSmaller.js
+++ src/includes/functions/$/isSmaller.js
@@ -2,6 +2,6 @@ isSmaller(val, not) {
 	if((this.val < val && not != false) || (not == false && !(this.val < val))) {
 		return true;
 	} else {
-		return `${this.val} isn't smaller than ${actual}`
+		return `${this.val} isn't smaller than ${val}`
 	}
 }
