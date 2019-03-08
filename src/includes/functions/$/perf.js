bdfc5d62ba17d6dc467949c313f28254824bb325
Arthur Guiot <arguiot@gmail.com>
2019-03-08 15:28:04 +0800

    🔨 Fixed perf.js

--- src/includes/functions/$/perf.js
+++ src/includes/functions/$/perf.js
@@ -7,7 +7,7 @@ perf(ms, not) {
 		const end = process.hrtime(start);
 		const time = Math.round((end[0] * 1000) + (end[1] / 1000000))
 		const average = time / 15
-		if (ms > average) {
+		if (ms < average) {
 			return not == true ? false : true
 		} else {
 			return `Your function runs in approximately ${average}ms, which is superior to ${ms}ms`
